da2e0f9697d2ce30d5301f499500cbc6c68f2c7d
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-15 22:41:18 +0530

    first commit

---
+++ class_task/vote.py
@@ -0,0 +1,14 @@
+location = input("hii, Enter your location : ")
+
+if (location =="bihar"):
+    
+    age = int(input("Enter your age :"))
+
+    if (age >=18):
+
+        print ("you are elgbile to vote :")
+    else:
+        print ("you are not eligbile to vote :")    
+else:
+    print ("you are not elgbile to vote:")        
+        

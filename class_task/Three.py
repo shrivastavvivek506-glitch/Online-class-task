a7bbba2e08d6c88db3cc27e29df4a43e734e155b
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-16 22:52:00 +0530

    Three number chaeck

---
+++ class_task/Three.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+c = int(input("Enter the third number : "))
+
+if a > b and a > c:
+    print("First number is greater :")
+elif b > a and b > c:
+    print("Second number is greater :") 
+else:    
+    print("Third number is greater :")

1608d6022a45a1483949a6ff64894115fc302279
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-19 22:56:08 +0530

    Remove methode

---
+++ class_task/Set_all_program/Remove.py
@@ -0,0 +1,15 @@
+print("//Example 1:")
+s1 = {2,3,4,5,6}
+s1.remove(6)
+print("After removing 6 from s1:", s1)
+
+print("//Example 2:")
+s2 = {"apple", "banana", "cherry"}
+s2.remove("cherry")
+print("After removing 'cherry' from s2:", s2)
+
+print("//Example 3:")
+s3 = {1,2,45,5}
+s3.remove(45)
+print("After removing 45 from s3:", s3)
+

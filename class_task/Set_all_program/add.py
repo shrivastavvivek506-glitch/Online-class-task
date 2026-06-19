61a174ce1bd0512fb32f5d6a2bf3d9e52a0f1e67
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-19 22:52:22 +0530

    Set methode :-add methode

---
+++ class_task/Set_all_program/add.py
@@ -0,0 +1,15 @@
+print("//Examole 1:")
+s1 = {2,3,4,5}
+s1.add(6)
+print("After adding 6 to s1:", s1)
+
+print("//Example 2:")
+s2 = {"apple", "banana", "cherry"}
+s2.add("orange")
+print("After adding 'orange' to s2:", s2)
+
+print("//Example 3:")
+s3 = {1,2,45,5}
+s3.add(5.66)
+print("After adding 5.66 to s3:",s3)
+

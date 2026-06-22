623782bee4dab48e30aade144ea609503478bd76
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-22 22:50:15 +0530

    Add for three number  in  function methond

---
+++ class_task/Set_all_program/Function Method/Sum.py
@@ -0,0 +1,11 @@
+print("//Example 1:")
+def add_three_numbers( a, b, c):
+    print("The sum of three numbers:", a + b + c)
+
+add_three_numbers(10, 34, 23)
+
+print("//Example 2:")
+def add_three_numbers( a, b, c):
+    print("the sum of three numbers is:", a + b + c)
+
+add_three_numbers(43, 555, 353.9)

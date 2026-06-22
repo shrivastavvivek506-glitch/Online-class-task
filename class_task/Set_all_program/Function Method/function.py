bd81b08ac8abb7ae1d7d4a3107661f669ad7f676
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-22 22:33:09 +0530

    function first program

---
+++ class_task/Set_all_program/Function Method/function.py
@@ -0,0 +1,15 @@
+def print_all_friends_names():
+    print("Rohit")
+    print("Vivek")
+    print("Aditya")
+    print("Gulam")
+
+print_all_friends_names()
+print("helloooooooooo")
+print_all_friends_names()
+
+What is a function 
+A function is a block of organized, reusable code that is used to perform a single, related action. Functions provide better modularity for your application and a high degree of code reusing. In Python, functions are defined using the `def` keyword, followed by the function name and parentheses `()`. The code block within every function starts with a colon `:` and is indented.
+
+What's the usecase of function
+Functions are used to break down complex problems into smaller, more manageable pieces. They allow you to reuse code, improve readability, and make your programs more organized. Functions can take inputs (parameters) and return outputs (results), making them versatile for a wide range of applications, such as performing calculations, processing data, or handling user interactions.

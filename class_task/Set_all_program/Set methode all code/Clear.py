db61bcdfe4a62a556736ef8397c4231dbf151fea
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-23 23:29:45 +0530

    clear method

---
+++ class_task/Set_all_program/Set methode all code/Clear.py
@@ -0,0 +1,13 @@
+print("//Example 1:")
+
+s1 = {1,2,34,5,}
+s1.clear()
+print("after clearing s1:", s1)
+
+# ----------------------------------------------
+
+print("//Example 2:")
+
+s2 = {32,54.6,76,98,"vivek"}
+s2.clear()
+print("after clearing s2:", s2)

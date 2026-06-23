38a4131c6cd87d9bb4d59cb900c6a88350b74c2d
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-06-23 23:27:36 +0530

    add

---
+++ class_task/Set_all_program/Set methode all code/Add.py
@@ -0,0 +1,9 @@
+x = {1,23,4,5,}
+x.add(54)
+print("after the add x:", x)
+
+# ------------------------------------------
+
+x = {"apple","banana","mango",}
+x.add("orange")
+print("after the add x:", x)

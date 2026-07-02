ba8042822e4cfe4ad97ddf57e6c820ea33cad869
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-02 15:50:38 +0530

    listing for indxing method

---
+++ class_task/Set_all_program/Function Method/Listing/List_indexing.py
@@ -0,0 +1,41 @@
+print("//example of list indexing")
+
+# Example 1
+my_list =[1,2,3,45,66]
+print(my_list[2])  # output: 3
+
+# Example 2
+my_list2 = ["apple", "banana", "cherry", "date"]
+print(my_list2[3]) # output: date
+
+# Example 3
+my_list3 = [10, 20, 30, 40, 50]
+print(my_list3[-1]) # output: 50
+
+# Example 4
+my_list4 = ["red", "green", "blue", "yellow"]
+print(my_list4[2]) # output: blue
+
+#Example 5
+my_list5 = [100, 200, 300, 400, 500]
+print(my_list5[0]) # output: 100
+
+#Example 6
+my_list6 = ["cat", "dog", "fish", "bird"]
+print(my_list6[-2]) # output: fish
+
+#Example 7
+my_list7 = [1,3,455,7,656]
+print(my_list7[-3]) # output: 455
+
+#Example 8
+my_list8 = ["Monday", "Tuesday", "Wednesday", "Thursday"]
+print(my_list8[1]) # output: Tuesday
+
+#Example 9
+my_list9 = [10, 20, 30, 40, 50]
+print(my_list9[4]) # output: 50
+
+#Example 10
+my_list10 = ["sun", "moon", "stars", "clouds"]
+print(my_list10[-4]) # output: sun

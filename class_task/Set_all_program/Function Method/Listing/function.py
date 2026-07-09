dc62ac0f97b17a223b75cb205358d73284f4ae6b
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-09 20:24:57 +0530

    Function check in Even or odd number

---
+++ class_task/Set_all_program/Function Method/Listing/function.py
@@ -0,0 +1,12 @@
+def check_even_odd(my_number):
+
+    if(my_number % 2== 0):
+        print("This is even number :")
+
+    else:
+       print("This is odd number :")
+
+
+print("hii , Vivek ")
+check_even_odd(10)
+

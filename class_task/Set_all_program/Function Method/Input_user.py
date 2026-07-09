0fbb02b4a4ca35c85511212c7b99c514e753ed88
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-09 20:32:13 +0530

    add

---
+++ class_task/Set_all_program/Function Method/Input_user.py
@@ -0,0 +1,11 @@
+def check_even_odd():
+    my_number=int(input("hii:, Vivek plese enter your number :"))
+    if(my_number % 2==0):
+        print("This number is even number :")
+
+    else:
+        print("This number is odd number :")
+
+check_even_odd()
+check_even_odd()
+check_even_odd()

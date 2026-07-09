09feb6822c3168c1847af79d774213192e98892f
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-09 20:55:28 +0530

    Positive and negative number to check for function method

---
+++ class_task/Set_all_program/Function Method/user_inpur.py
@@ -0,0 +1,13 @@
+def positive_negative():
+    my_number =int(input("hii:, Vivek plese enter your number :"))
+
+    if(my_number > 0):
+        print("This is positive number :", my_number)
+    elif(my_number < 0):
+        print("This is negative number :", my_number) 
+    else:
+        print("This is zero :", my_number)
+
+positive_negative()
+positive_negative()
+positive_negative()

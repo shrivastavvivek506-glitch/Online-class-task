5e6e1c4128417ddaf60cb46f7dd20b468291c351
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-09 20:47:30 +0530

    add

---
+++ class_task/Set_all_program/Function Method/positive.py
@@ -0,0 +1,13 @@
+def positive_negative(my_number):
+    if(my_number > 0):
+        print("This is positive number :", my_number)
+
+    elif(my_number < 0):
+        print("This is negative number :", my_number)
+    else:
+        print("This is zero :", my_number)
+
+print("hii , Vivek")
+positive_negative(10)
+positive_negative(-32)
+positive_negative(0)

e86393f95b1183562ae78f75774ad03d60081be9
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-11 23:46:14 +0530

    Age limi for

---
+++ class_task/Age_limit.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age :"))
+if age >=18 and age <=60:
+    print("you are for eligible for job :")
+elif age >60:
+    print("your age is very high so, you are not eligible for job :")
+else:   
+    print("you are not eligible for job :")

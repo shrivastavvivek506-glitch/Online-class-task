930238c70ab686bf14619c7db888f9e99404e87a
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-11 22:39:55 +0530

    Class  method in code

---
+++ class_task/oops methode/simple.py
@@ -0,0 +1,50 @@
+print ("//Example 1:")
+
+class Student:
+    name = "Jhony"
+    age = 20
+x = Student()
+print(x.name)
+print(x.age)
+#-------------------------------------------------
+print("//Example 2:")
+
+class fruit:
+    name = "mango"
+    color = "yellow"
+y = fruit()
+print(y.name)
+print(y.color)
+#-------------------------------------------------
+print("//Example 3:")
+class car:
+    name = "BMW"
+    model = "X5"
+    year = 2020
+z = car()
+print(z.name)
+print(z.model)
+print(z.year)
+
+#-------------------------------------------------
+print("//Example 4:")
+class animal:
+    name = "Dog"
+    color = "brown"
+    breed = "labrador"
+    age = 5
+a = animal()
+print(a.name)
+print(a.color)
+print(a.breed)
+print(a.age)
+#-------------------------------------------------
+print("//Example 5:")
+class book:
+    title = "The Great Gatsby"
+    author = "F. Scott Fitzgerald"
+    year = 1925
+b = book()
+print(b.title)
+print(b.author)
+print(b.year)    

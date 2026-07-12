1e9d66e9ba97b0170bb94bc5d928dda0fe01a3c5
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-12 23:52:21 +0530

    inheritance method

---
+++ class_task/Set_all_program/Function Method/Inc.py
@@ -0,0 +1,13 @@
+print("//Example 1:")
+class person():
+    name = "vivek"
+    age = 19
+class student(person):
+    roll_no = 250110341
+    mobile_no = 1234567890
+
+x = student()
+print(x.name)
+print(x.age)
+print(x.roll_no)
+print(x.mobile_no)

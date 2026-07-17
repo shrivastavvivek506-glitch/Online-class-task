24b08bc2ea73a37c00f1b4aaaa87f30478711533
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-17 19:57:07 +0530

    add

---
+++ class_task/oops methode/Example.py
@@ -0,0 +1,12 @@
+print("//Example 1:")
+
+class student():
+    name="Vivek Shrivastav"
+    course="BCA"
+    def student_details(self):
+        print("student all info")
+
+s = student()
+print(s.name)
+print(s.course)
+s.student_details

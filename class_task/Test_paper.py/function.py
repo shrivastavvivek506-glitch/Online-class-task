890d274c48740fb4e35e2fd5f776c3fd6314cbb8
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-22 21:12:11 +0530

    add

--- class_task/Test_paper.py/function.py
+++ class_task/Test_paper.py/function.py
@@ -1,9 +1,20 @@
-def student_info():
-    name = "vivek shrivatav"
-    course = "BCA"
-    roll_no = "1223231"
+# def student_info():
+#     name = "vivek shrivatav"
+#     course = "BCA"
+#     roll_no = "1223231"
 
-student_info()
-print("Name:", name)
-print("Course:", course)
-print("Roll_no:", roll_no)
+# student_info()
+# print("Name:", name)
+# print("Course:", course)
+# print("Roll_no:", roll_no)
+
+
+class college:
+    college_name = "IES UNIVERSITY"
+    branch = "CSE"
+    fee ="1L"
+
+c= college()
+print(c.college_name)
+print(c.branch)
+print(c.fee)

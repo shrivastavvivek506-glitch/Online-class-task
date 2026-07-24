9ddb167a262a7324c9c4e335a84684c5e450ce09
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-24 18:37:31 +0530

    overrining methode

---
+++ class_task/Polymoaphism/overriding.py
@@ -0,0 +1,13 @@
+class vivek:
+    def assets(self):
+        print("vivek assets")
+
+class aditya:
+    def assets(self):
+        print("aditya assets")
+
+vivek_obj = vivek()
+aditya_obj = aditya()
+
+vivek_obj.assets()
+aditya_obj.assets()

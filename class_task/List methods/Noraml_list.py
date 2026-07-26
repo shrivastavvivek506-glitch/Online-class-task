471cc157fdd1f6bd7504a17fa3d0799351f33a5e
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-07-26 23:02:03 +0530

    Listing and Slicing  Include Two Methodes

---
+++ class_task/List methods/Noraml_list.py
@@ -0,0 +1,27 @@
+Normal listing 
+my_list = ["Vivek",30,"Sonakshi",24,"Fusture"]
+print(my_list)
+
+------------------------------------------------
+List Slicing(Positive Indexing )
+print("//Example 1:")
+my_list = ["Vivek",30,"Sonakshi",24,"Fusture"]
+print(my_list[0:3])
+
+print("// Example 2:")
+my_list = ["Vivek",30,"Sonakshi",24,"Fusture"]
+print(my_list[0:6:2])
+
+
+----------------------------------------------------
+Negative Indexing 
+print("// Example 1:")
+
+my_list = ["Vivek",30,"Sonakshi",24,"Fusture"]
+print(my_list[-3:-2])
+
+print("// Examle 2:")
+
+my_list = ["Vivek",30,"Sonakshi",24,"Fusture"]
+print(my_list[0:-2:2])
+

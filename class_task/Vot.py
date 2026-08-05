e1dff7146944810f570ca768f077b7d1b503f5e8
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-08-05 23:33:56 +0530

    add

---
+++ class_task/Vot.py
@@ -0,0 +1,12 @@
+location = input("Enter your location :")
+if location == "bihar":
+    age = int(input("Enter your age : "))
+
+    if age >= 18:
+
+        print("You are eligible to vote :")
+    elif age < 18:
+                print("You are not eligible to vote :")
+
+else:
+        print("You are not from bihar, so you are not eligible to vote :") 

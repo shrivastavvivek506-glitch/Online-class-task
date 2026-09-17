2d97172cefeaa1ee7127ee141b694e63a670db43
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-09-17 20:26:52 +0530

    inhertence

---
+++ class_task/Online Test/Inhertence.py
@@ -0,0 +1,16 @@
+#Single Inheritance
+
+class model():
+    car_mobel = "BMW"
+    seral_no = "BTM3575TWNC7CCL"
+class owner( model):
+    owner_name = "Vivek shrtivatav"
+    mobile_no = "797399462"
+
+owner_obj = owner()
+
+print(owner_obj.car_mobel)
+print(owner_obj.owner_name)
+print(owner_obj.seral_no)
+print(owner_obj.mobile_no)
+

d17f9db534586aade1b2ceff631f2689cb441c16
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-09-18 23:53:13 +0530

    Multiple Inhertance

---
+++ class_task/Online Test/multiple.py
@@ -0,0 +1,12 @@
+class Sanjana():
+    Sbs =  " B.Com Student"
+
+class Aditya(Sanjana):
+    Company_name = "Vivo"
+
+
+class Vivek(Aditya):
+    Privact_Ltd = "Ayansh Dhabha"
+
+Vivek_obj = Vivek()
+print(Vivek_obj.Company_name)
